f50ff97d54951e741b42b85ba489b2e6de4d106e
Felipe Beserra <felipebeserra25@gmail.com>
2023-08-15 07:41:03 -0300

    Create popshell.sh

---
+++ scripts/popshell.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# POP_OS Shell -> Tiling and Shortcuts
+# Tutorial: https://github.com/pop-os/shell#installation
+
+sudo apt install git node-typescript make
+cd ~/.local
+git clone https://github.com/pop-os/shell.git --depth 1
+cd ./shell
+
+# THIS WILL SHUTDOWN YOUR COMPUTER!
+make local-install
